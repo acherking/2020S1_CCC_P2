dd46343d89cbd3eaa47d57706c46c41a1facfb07
Sven <wzxfate@gmail.com>
2020-05-26 21:30:31 +1000

    change mode +x

--- ansible/add-couchdb-node.sh
+++ ansible/add-couchdb-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IP1=xxx
-IP4=xxx
+IP1=172.26.132.113
+IP4=172.26.130.93
 
 curl -X POST -H "Content-Type: application/json" http://admin:123456@${IP1}:5984/_cluster_setup -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "admin", "password":"123456", "port": 5984, "node_count": "4", "remote_node": "${IP4}", "remote_current_user": "admin", "remote_current_password": "123456" }'
 curl -X POST -H "Content-Type: application/json" http://admin:123456@${IP1}:5984/_cluster_setup -d '{"action": "add_node", "host":"${IP4}", "port": 5984, "username": "admin", "password":"123456"}'
